61797f4fd4fc84f9d83a2d5b70dd11862263b1ba
MateuszBaran <2meffiki>
2018-11-16 10:27:24 +0100

    first commit added main class added the list of exercises

---
+++ src/main/java/Exercises/App.java
@@ -0,0 +1,49 @@
+package Exercises;
+
+
+import java.util.Scanner;
+
+/**
+ * Hello world! ... and some other exercises
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+
+    {
+//        System.out.println( "Hello World!" );
+
+//        int A = 50;
+//        int B = 3;
+//        int result = 5 + ((15 / 3) * 2) - (8 % 3);
+//        System.out.println(result);
+
+//        System.out.println("Enter first number:");
+//        Scanner sc1 = new Scanner(System.in);
+//        int firstNumber = sc1.nextInt();
+//
+//        System.out.println("Enter second number:");
+//        Scanner sc2 = new Scanner(System.in);
+//        int secondNumber = sc2.nextInt();
+//
+//        int resultAddition = firstNumber + secondNumber;
+//        int resultSubtraction = firstNumber - secondNumber;
+//        int resultMultiplication = firstNumber * secondNumber;
+//        int resultDivision = firstNumber / secondNumber;
+//        int resultModulo = firstNumber % secondNumber;
+//
+//        System.out.println(firstNumber + " + " + secondNumber + " equals " + resultAddition);
+//        System.out.println(firstNumber + " - " + secondNumber + " equals " + resultSubtraction);
+//        System.out.println(firstNumber + " * " + secondNumber + " equals " + resultMultiplication);
+//        System.out.println(firstNumber + " / " + secondNumber + " equals " + resultDivision);
+//        System.out.println(firstNumber + " % " + secondNumber + " equals " + resultModulo);
+
+        Scanner input = new Scanner(System.in);
+
+
+
+    }
+
+}
