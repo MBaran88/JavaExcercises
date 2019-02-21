c78c8b267085593d62ebf19b3222531bef12e91f
MateuszBaran <2meffiki>
2019-02-21 16:08:46 +0100

    text calc started

---
+++ src/main/java/Exercises/Addition.java
@@ -0,0 +1,8 @@
+package Exercises;
+
+public class Addition {
+
+    public double addition (double num1, double num2) {
+     return num1 + num2;
+    }
+}
--- src/main/java/Exercises/App.java
+++ src/main/java/Exercises/App.java
@@ -1,10 +1,30 @@
 package Exercises;
 
+import java.util.Scanner;
+
 /**
  * Hello world! ... and some other exercises
  */
 public class App {
     public static void main(String[] args){
+
+        int switchNumber = 0;
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Wybierz numer odpowiadający działaniu obok: ");
+        System.out.println("Dodawanie 1");
+        System.out.println("Odejmowanie 2");
+        System.out.println("Mnożenie 3");
+        System.out.println("Dzielenie 4");
+
+        int selectedOperation = reader.nextInt();
+
+        switch (switchNumber) {
+            case 1:
+                Addition addition = new Addition();
+        }
+
+
 
 }
 }
